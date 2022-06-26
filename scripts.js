38c99900e0be5728b26b4bedca196f55bc6423ff
Lucas Castellani <lucas100castellani@gmail.com>
2022-06-25 22:27:21 -0300

    Adicionado products na view

--- scripts.js
+++ scripts.js
@@ -64,3 +64,12 @@ var products = [
     }
 ];
  
+const SelfServiceMachine = {
+    data() {
+        return {
+            products
+        }
+    }
+};
+
+Vue.createApp(SelfServiceMachine).mount('#app');
